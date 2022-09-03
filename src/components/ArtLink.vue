3f7044938c6138ef6582b6e9b9a84ec33566e854
Unstriped <stefanmagnusson90@gmail.com>
2022-09-03 13:09:02 +0200

    changed grid

--- src/components/ArtLink.vue
+++ src/components/ArtLink.vue
@@ -1,9 +1,9 @@
 <template>
   <router-link class="h-full w-full flex" :to="'/art/' + title">
     <div
-      class="w-full border-2 border-slate-100 drop-shadow-sm p-4 box-content bg-slate-50 hover:bg-gray-100 transition-all relative overflow-hidden"
+      class="w-full p-4 box-content hover:bg-slate-50 transition-all relative overflow-hidden"
     >
-      <h3 class="capitalize text-2xl mb-4 text-center">{{ title }}</h3>
+      <h3 class="capitalize text-2xl my-4 text-center">{{ title }}</h3>
       <img
         class="max-w-full max-h-72 mx-auto"
         :src="require(`../assets/art/` + formattedFolder + image + '')"
